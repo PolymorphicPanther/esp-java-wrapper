217f5a64e69e48c779fabe64ae05d2b6d919b0dc
Luvy <luveshenp@gmail.com>
2023-03-29 21:21:49 +0200

    Revert to working github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,6 @@
-import org.gradle.kotlin.dsl.java
-
 plugins {
     id("java")
     id("maven-publish")
-    id("signing")
-    id("org.jetbrains.dokka") version "1.4.20"
-    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
 
 group = "org.jesp"
@@ -13,26 +8,6 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-java{
-    withSourcesJar()
-    withJavadocJar()
-}
-
-signing {
-    val signingKey = providers
-            .environmentVariable("GPG_SIGNING_KEY")
-            .forUseAtConfigurationTime()
-    val signingPassphrase = providers
-            .environmentVariable("GPG_SIGNING_PASSPHRASE")
-            .forUseAtConfigurationTime()
-    if (signingKey.isPresent && signingPassphrase.isPresent) {
-        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
-        val extension = extensions
-                .getByName("publishing") as PublishingExtension
-        sign(extension.publications)
-    }
 }
 
 dependencies {
@@ -52,14 +27,6 @@ publishing {
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-        maven {
-            name = "OSSRH"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
             }
         }
     }
