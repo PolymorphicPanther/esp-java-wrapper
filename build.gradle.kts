6c128d85773961db335219259f5519146619fffe
Luvy <luveshenp@gmail.com>
2023-03-29 21:19:26 +0200

    fix repository credentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/PolymorphicPanther/esp-java-wrapper")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
             }
         }
     }
