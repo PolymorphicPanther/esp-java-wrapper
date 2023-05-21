e71c60fd7301f7771ebb53b20c7f3156c2eaa942
Luvy <luveshenp@gmail.com>
2023-05-21 18:59:32 +0200

    Try to publish and not sign to OSSSRH

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("maven-publish")
+
 }
 
 group = "org.jesp"
@@ -27,6 +28,14 @@ publishing {
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+        maven {
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
             }
         }
     }
